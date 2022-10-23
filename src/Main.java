70b5f5d6a9ea28af6f5e8ba4a58fd9ebb8c7077d
asadev1411 <kaisar1990@mail.ru>
2022-10-23 11:49:44 +0500

    Zadacha №1 vypolnena

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
 
+        //Задача №1
+        System.out.println("Задача №1");
+        byte nomerPojarnoiSlujby = 101;
+        System.out.println("Значение переменной nomerPojarnoiSlujby с типом byte равно " + nomerPojarnoiSlujby +".");
+        short vKarmaneNalichnyh = 20500;
+        System.out.println("Значение переменной vKarmaneNalichnyh с типом short равно " + vKarmaneNalichnyh +"руб.");
+        int aHotelosBy = 1000000;
+        System.out.println("Значение переменной aHotelosBy с типом short равно " + aHotelosBy +"руб.");
+        long doKoncaJiznyNeUspelByPotratitNaLichnyeNujdy = 1000000000000L;
+        System.out.println("Значение переменной doKoncaJiznyNeUspelByPotratitNaLichnyeNujdy с типом short равно " + doKoncaJiznyNeUspelByPotratitNaLichnyeNujdy +"руб.");
+        float daje = 0.1f;
+        System.out.println("Значение переменной daje с типом float равно " + daje +" часть.");
+        double VeroyatnostChtoUspeyu = 0.0000000000000001;
+        System.out.println("Значение переменной VeroyatnostChtoUspeyu с типом double равно " + VeroyatnostChtoUspeyu +" процентов.");
+
+        //Задача №2
+        System.out.println("Задача №2");
     }
 }
